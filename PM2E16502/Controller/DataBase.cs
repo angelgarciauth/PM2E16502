6c14a9320b6c98c61db76f609031785c2221d0da
ang <ag888927@gmail.com>
2022-06-18 14:55:58 -0600

    Configuracion del proyecto

---
+++ PM2E16502/Controller/DataBase.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using PM2E16502.Models;
+using SQLite;
+
+namespace PM022PP0122.Controller
+{
+    public class DataBase
+    {
+        readonly SQLiteAsyncConnection dbase;
+        /*Constructor de clase*/
+        
+        public DataBase(string dbpath) {
+            dbase = new SQLiteAsyncConnection(dbpath);
+            //creacion de las tablas de la base de datos
+            dbase.CreateTableAsync<Sitios>(); //creando tabla de empleados
+
+        }
+
+        #region Operaciones
+
+        //CRUD -CREATE  -READ  -UPDATE  -DELETE
+
+
+        //Create
+        public Task<int> SiteSave(Sitios site)
+        {
+            if(site.id != 0)
+            {
+                return dbase.UpdateAsync(site);
+            }
+            else
+            {
+                return dbase.InsertAsync(site);
+            }
+        }
+
+        //Read
+        public Task<List<Sitios>> getListSite()
+        {
+            return dbase.Table<Sitios>().ToListAsync();
+        }
+
+        //Read v2
+        public Task<Sitios> getSite(int pid)
+        {
+            return dbase.Table<Sitios>()
+                .Where(i => i.id == pid)
+                .FirstOrDefaultAsync();
+        }
+
+        //Delete
+
+        public Task<int> DeleteSite(Sitios site)
+        {
+            return dbase.DeleteAsync(site);
+        }
+
+        #endregion Operaciones
+
+      
+    }
+}
---
+++ PM2E16502/Models/Sitios.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace PM2E16502.Models
+{
+    public class Sitios
+    {
+        [PrimaryKey, AutoIncrement]
+        public int id { get; set; }
+        public Byte[] foto { get; set; }
+        public Double latitud { get; set; }
+        public Double longitud { get; set; }
+        public String descripcion { get; set; }
+
+
+        
+    }
+}
